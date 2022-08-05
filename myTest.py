badaf30878b5afab34620bf00807defc11ff1c3a
L-shuai <1472174772@qq.com>
2022-08-05 03:51:10 +0000

    Initial commit

---
+++ myTest.py
@@ -0,0 +1,7 @@
+
+#data/summarize/python/test.jsonl
+# /headless/qhj/CodeT5-main2/data/summarize/python/test.jsonl
+if __name__=="__main__":
+    with open('/data/summarize/python/test.jsonl', 'r') as f:
+        for line in f:
+            print("yihang")
